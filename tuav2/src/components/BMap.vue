8a66d715852f3e342f024ab7247d4bce59bcbd77
姜琳 <490052745@qq.com>
2019-01-07 23:30:22 +0800

    add

--- tuav2/src/components/BMap.vue
+++ tuav2/src/components/BMap.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="map">
-    <div id="allmap"></div>
+  <div id="BMap" style="width:100%;height:243px">
+    <div id="allmap" style="height:100%;"></div>
   </div>
 </template>
 <script>
   export default{
-    name:'map',
+    name:'app',
     mounted(){
 　　　this.baiduMap();
     },
     methods:{
       baiduMap: function(){
         var map = new BMap.Map('allmap');
-        var point = new BMap.Point(22.5511344137,113.9550486789);
+        var point = new BMap.Point(113.9550486789,22.5511344137);
         var marker = new BMap.Marker(point);
         map.addOverlay(marker);
         map.centerAndZoom(point,15);
-          
+        map.setCurrentCity("深圳");  
         map.enableScrollWheelZoom();
         map.enableContinuousZoom();
       }
