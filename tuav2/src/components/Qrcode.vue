b29ba984f16739c5b2001aba68474905983914dd
姜琳 <490052745@qq.com>
2019-01-08 18:27:58 +0800

    qrcode

---
+++ tuav2/src/components/Qrcode.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="qrcode">
+  <qrcode-vue :value="link" :size="size" level="H"></qrcode-vue>
+</div>
+</template>
+<script>
+import QrcodeVue from 'qrcode.vue';
+ 
+export default {
+    props:['link'],
+    data() {
+        return {
+        size: 119,
+        value:'',
+        background:'#444'
+        }
+    },
+    components: {
+        QrcodeVue
+    }
+}
+</script> 
--- tuav2/src/views/workdetail.vue
+++ tuav2/src/views/workdetail.vue
@@ -20,16 +20,18 @@
     .detail-infos .author-title {margin-top: 60px}
     .detail-infos .author-list{ width: 500px;margin: 10px 0 60px;color:#969696;}
     .detail-infos .author-list li{ font-size: 14px; margin-bottom: 8px; line-height: 1.5}
-    .detail-infos .author-list li span{ }
-
+    .audio_container {width: 614px;height: 614px;position: relative;background-size: cover;background-repeat: no-repeat;background-position: center center;}
     .audio-frame{ position: absolute;bottom: 0;margin: 20px;}
+    .qrcode_container {text-align: center; width:121px;height:140px;position: absolute;bottom: 120px;left: 400px;}
+    .qrcode_container .qrcode {width: 121px;height: 121px;border: 1px solid #606060;}
+    .qrcode_container .f12 {color:#969696;}
 </style>
 
 <template>
     <div>
         <top-nav></top-nav>
         <second-nav></second-nav>
-        <body-frame class="flex">
+        <body-frame class="flex" style="padding:0 60px">
             <div class="detail-infos">
                 <h3>{{detailData.title}}</h3>
                 <div class="sub-title f20">{{detailData.title_ext}}</div>
@@ -48,17 +50,15 @@
                     <span v-if="detailData.cate2 != ''">{{detailData.catename}} / {{detailData.catename2}}</span>
                     <div style="margin-top:8px">{{new Date(parseInt(detailData.ctime)).format('MM/yyyy')}}</div>
                 </div>
+                <div class="qrcode_container tr" v-if='detailData.h5_link'>
+                    <qrcode :link='detailData.h5_link'></qrcode>
+                    <div class="mt10 f12">扫码查看案例</div>
+                </div>
             </div>
-            <div class="swiper-frame"  v-if="!showVideo">
+            <div class="swiper-frame"  v-if="!showVideo && !detailData.audio_link">
                 <swiper :options="swiperOption" ref="mySwiper">
-                    <swiper-slide v-for="(item,index) in bannerList" :key="index" :style="{backgroundImage:'url('+domain_url+item+')'}" :class="{'audio':detailData.audio_link != ''}">
-                    <div class="audio-frame" v-if="detailData.audio_link != ''">
-                        <audio-view
-                            v-if="detailData.audio_link != ''"
-                            :src="domain_url+detailData.audio_link"
-                            :title="detailData.audio_name">
-                        </audio-view>
-                    </div>
+                    <swiper-slide v-for="(item,index) in bannerList" :key="index" :style="{backgroundImage:'url('+domain_url+item+')'}">
+                   
                     </swiper-slide>
                 </swiper>
                  <div class="arrow left">
@@ -74,9 +74,23 @@
                         </a>
                     </div>
             </div>
+            <div class="audio_container" v-if="detailData.audio_link != ''" :style="{backgroundImage:'url('+domain_url+bannerList[0]+')'}">
+                <div class="audio-frame">
+                    <audio-view
+                        v-if="detailData.audio_link != ''"
+                        :src="domain_url+detailData.audio_link"
+                        :title="detailData.audio_name">
+                    </audio-view>
+                </div>
+            </div>
+             
             <video-view  v-if="showVideo"
                          :vid="vid"
                          :postImg="vPostImg"></video-view>
+        
+
+        
+        
         </body-frame>
         <bottom-nav></bottom-nav>
     </div>
@@ -90,9 +104,10 @@
     import BodyFrame from '@/components/BodyFrame.vue'
     import AudioView from '@/components/AudioView.vue'
     import VideoView from '@/components/VideoView.vue'
+    import Qrcode from '@/components/Qrcode.vue'
     export default{
         name: 'App',
-        components:{TopNav,SecondNav,BottomNav,WorkItem,BodyFrame,AudioView,VideoView},
+        components:{TopNav,SecondNav,BottomNav,WorkItem,BodyFrame,AudioView,VideoView,Qrcode},
         data(){
             let self = this;
             return{
